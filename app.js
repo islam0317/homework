93336691be06aba8705509ee537380c2d811d933
islam0317 <155236290+islam0317@users.noreply.github.com>
2024-02-28 02:11:49 -0800

    sss

--- app.js
+++
@@ -1,1 +0,0 @@
-alert('islam sheikh')
---
+++ chapters/app.js
@@ -0,0 +1,13 @@
+
+// chapter no 01//
+alert('welcome to js land...')
+
+
+alert('happy coading!')
+
+
+alert('hello... i can run JS though my web browser console')
+
+
+
+
